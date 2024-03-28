a4827da0e7e9cf266d086c6a0da0fb0bb974f96e
P1ST-0N <luckylionia@gmail.com>
2024-03-28 16:49:56 +0200

    added all files & dps

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "../../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.module.css";
+
+import "../../../node_modules/modern-normalize/modern-normalize.css";
+
+import AOS from "aos";
+AOS.init();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useAuth.js
@@ -0,0 +1,4 @@
+import { useSelector } from "react-redux";
+// import redux
+
+export const useAuth = () => {};
