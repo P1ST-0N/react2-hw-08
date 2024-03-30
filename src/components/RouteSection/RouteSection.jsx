19dc4921327e9806cd76946fa1d84c9950c3c54d
P1ST-0N <luckylionia@gmail.com>
2024-03-30 18:14:38 +0200

    added TitleDocument

--- src/components/RouteSection/RouteSection.jsx
+++ src/components/RouteSection/RouteSection.jsx
@@ -1,8 +1,8 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout";
-// import
-// import
+import RestrictedRoute from "../RestrictedRoute";
+import PrivateRoute from "../PrivateRoute";
 
 //pages
 
@@ -11,11 +11,38 @@ const RouteSection = () => {
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
+          <Route>Home</Route>
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                // component={<Register />}
+              />
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                //   component={<Login />}
+              />
+            }
+          ></Route>
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute
+                redirectTo="/login"
+                //   component={<Contacts />}
+              />
+            }
+          />
+          <Route
+            path="*"
+            //   element={<NotFoundPage />}
+          />
         </Route>
       </Routes>
     </>
